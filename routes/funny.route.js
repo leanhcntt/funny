8d9074c6fbe05c52833c26d9bf7402afc5671cb8
ltanh <leanhcntt@gmail.com>
2021-08-15 14:47:05 +0700

    FNY-1: return message hehe

--- routes/funny.route.js
+++ routes/funny.route.js
@@ -5,4 +5,9 @@ router.get('/', async (req, res) => {
     res.status(200).json({"Message": "Funny"});
 });
 
+// FNY-1 + API 1: Hehe
+router.get('/Hehe', async (req, res) => {
+    res.status(200).json({"Message": "Hehe"});
+});
+
 module.exports = router;
